c2dd7a5faeed12cbbad05334556d46fbfa3e3bbc
Federico Mameli <federico_mameli@yahoo.com>
2023-04-12 20:48:59 +0200

    feat(ui): create button component

---
+++ components/UiButton.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import { variantsClasses } from '~/utils/style'
+
+export interface Props {
+  variant?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  variant: 'primary'
+})
+
+</script>
+
+<template>
+  <button
+    class="flex items-center py-2 px-4 rounded transition-all duration-300 ease-out transform hover:translate-y-[1px] hover:shadow uppercase font-semibold"
+    :class="[variantsClasses.get(variant)]"
+  >
+    <slot />
+  </button>
+</template>
