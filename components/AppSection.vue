8c5fe6ec4f4a42dd15e944218e05a5cb26c44908
Federico Mameli <federico_mameli@yahoo.com>
2023-04-12 20:48:36 +0200

    feat: create basic section component

---
+++ components/AppSection.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <section class="py-5">
+    <h2 class="mb-5 underline uppercase underline-offset-4">
+      {{ title }}
+    </h2>
+
+    <slot />
+  </section>
+</template>
