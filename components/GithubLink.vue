3147bd44f79c3214e271a23c3812dc015f79864f
Federico Mameli <federico_mameli@yahoo.com>
2023-04-12 20:50:20 +0200

    feat: create Github link button

---
+++ components/GithubLink.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+export interface Props {
+  label?: string
+  link: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <a
+    class="inline-flex items-center gap-1 px-2 py-1 text-sm transition-colors duration-150 ease-linear rounded bg-slate-300 hover:bg-slate-400 hover:shadow"
+    :href="link"
+    target="blank"
+  >
+    <app-icon icon="github" />
+    <slot>{{ label }}</slot>
+  </a>
+</template>
