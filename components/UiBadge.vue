194f50a7abc7144be9b6ff44873b6f1f15455a1a
Federico Mameli <federico_mameli@yahoo.com>
2023-04-12 20:49:16 +0200

    feat(ui): create badge component

---
+++ components/UiBadge.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+defineProps<{
+  text?: string;
+}>()
+</script>
+
+<template>
+  <span class="rounded-full leading-none px-3 py-1 text-sm font-semibold">
+    <slot>{{ text }}</slot>
+  </span>
+</template>
