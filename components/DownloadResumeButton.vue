fd55251ac0310384ab2f8ce0c05fd6a4a887f8f3
Federico Mameli <federico_mameli@yahoo.com>
2023-04-13 10:31:42 +0200

    feat: create download resume button

---
+++ components/DownloadResumeButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <ui-button id="resume-btn" class="gap-2">
+    <app-icon icon="file-download" class="text-xl" />
+    <span>Download Resume</span>
+  </ui-button>
+</template>
+
+<style lang="postcss" scoped>
+  #resume-btn span{
+    @apply opacity-0 w-0 overflow-hidden whitespace-nowrap transition-all duration-200 ease-out;
+  }
+
+  #resume-btn:hover span{
+    @apply w-full opacity-100;
+  }
+</style>
