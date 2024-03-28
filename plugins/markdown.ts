69bdc1a02adce5efd2a31828c632175eae7e6ad9
Federico Mameli <federico_mameli@yahoo.com>
2024-03-28 11:44:29 +0100

    feat(directives): add markdown directive

---
+++ plugins/markdown.ts
@@ -0,0 +1,21 @@
+import insane from 'insane'
+import { marked } from 'marked'
+
+function parse (markdownText: string): string {
+  const html = marked(markdownText, { async: false }) as string
+  return insane(html)
+}
+
+export default defineNuxtPlugin(({ vueApp }) => {
+  vueApp.directive('md', {
+    mounted (el, { value }) {
+      el.innerHTML = parse(value)
+    },
+
+    updated (el, { value, oldValue }) {
+      if (value !== oldValue) {
+        el.innerHTML = parse(value)
+      }
+    }
+  })
+})
