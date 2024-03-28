b277a5fbf3dbd0b0fea9b205f9492fd2c6b9db87
Federico Mameli <federico_mameli@yahoo.com>
2024-03-28 11:43:51 +0100

    chore: add insane types

---
+++ types/insane.d.ts
@@ -0,0 +1,20 @@
+// declare module 'insane';
+
+declare module 'insane' {
+
+  interface InsaneFilterToken {
+    tag: string
+    attrs: Record<string, unknown>
+  }
+
+  interface InsaneOptions {
+    allowedAttributes: Record<string, string[]>
+    allowedClasses: Record<string, string[]>
+    allowedSchemes: string[]
+    allowedTags: string[]
+    filter?: (token: InsaneFilterToken) => boolean
+    transformText?: (text: string) => string
+  }
+
+  export default function (html: string, options?: InsaneOptions, strict?: boolean): string
+}
