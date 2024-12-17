1a592b6f3a86380a5c233a2b02acd852fdd1c7ea
Federico Mameli <federico_mameli@yahoo.com>
2024-12-17 14:04:14 +0000

    feat: migrate to nuxt 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-10-07',
+  compatibilityDate: "2024-10-07",
 
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n', '@nuxt/eslint'],
+  future: {
+    compatibilityVersion: 4,
+  },
 
-  css: [
-    '@/assets/styles/typography.css',
-    '@/assets/styles/base.css'
-  ],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n", "@nuxt/eslint"],
+
+  css: ["@/assets/styles/typography.css", "@/assets/styles/base.css"],
 
   devtools: {
-    enabled: true
+    enabled: true,
   },
 
   i18n: {
-    vueI18n: './config/i18n.config.ts'
+    vueI18n: "./config/i18n.config.ts",
   },
 
   build: {
-    transpile: ['primevue']
-  }
-
-})
+    transpile: ["primevue"],
+  },
+});
