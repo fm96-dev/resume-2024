9098822505e67c44d4e849b44825226ee7c08487
Federico Mameli <federico_mameli@yahoo.com>
2024-12-17 14:06:59 +0000

    refactor: move lang folder to app

--- config/i18n.config.ts
+++ config/i18n.config.ts
@@ -1,8 +1,8 @@
-import en from '~~/lang/en'
-import it from '~~/lang/it'
+import en from "~/lang/en";
+import it from "~/lang/it";
 
 export default defineI18nConfig(() => ({
   legacy: false,
-  locale: 'it',
-  messages: { en, it }
-}))
+  locale: "it",
+  messages: { en, it },
+}));
