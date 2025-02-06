1faaf43f5c04decd250d64eebe1b62a500f98d61
Federico Mameli <federico_mameli@yahoo.com>
2025-02-06 12:00:43 +0000

    refactor: standardize quotes and update technology color mappings

--- app/components/Timelines/ExperienceTimeline.vue
+++ app/components/Timelines/ExperienceTimeline.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable vue/no-v-html -->
 <script lang="ts" setup>
-import Timeline from 'primevue/timeline'
+import Timeline from "primevue/timeline";
 
 interface TimelineEvent {
   title: string;
@@ -10,38 +10,56 @@ interface TimelineEvent {
   description: string;
 }
 
-const { t } = useI18n()
+const { t } = useI18n();
 
 const events = computed<TimelineEvent[]>(() => [
   {
-    title: 'Full Stack Web Developer',
-    company: 'Smart Contact / Meglioquesto Digital',
-    time: t('experienceEvents.0.time'),
+    title: "Full Stack Web Developer",
+    company: "Smart Contact / Meglioquesto Digital",
+    time: t("experienceEvents.0.time"),
     technologies: [
-      'JavaScript', 'Vue', 'TailwindCSS',
-      'Laravel', 'PHP', 'MySQL', 'MongoDB',
-      'Cypress', 'AWS', 'Webpack', 'Vite.js', 'Docker'
+      "JavaScript",
+      "TypeScript",
+      "Vue",
+      "TailwindCSS",
+      "Laravel",
+      "PHP",
+      "MySQL",
+      "MongoDB",
+      "Cypress",
+      "AWS",
+      "Webpack",
+      "Vite.js",
+      "Docker",
     ],
-    description: t('experienceEvents.0.description')
+    description: t("experienceEvents.0.description"),
   },
   {
-    title: 'Front End Web Developer',
-    company: 'Smart Contact',
-    time: t('experienceEvents.1.time'),
-    technologies: ['JavaScript', 'Vue', 'Bootstrap', 'CSS', 'SCSS', 'Node.js', 'Jest', 'Webpack', 'Vite.js'],
-    description: t('experienceEvents.1.description')
-
-  }
-])
-
+    title: "Front End Web Developer",
+    company: "Smart Contact",
+    time: t("experienceEvents.1.time"),
+    technologies: [
+      "JavaScript",
+      "Vue",
+      "Bootstrap",
+      "CSS",
+      "SCSS",
+      "Node.js",
+      "Jest",
+      "Webpack",
+      "Vite.js",
+    ],
+    description: t("experienceEvents.1.description"),
+  },
+]);
 </script>
 
 <template>
   <Timeline :value="events">
-    <template #content="{item}">
-      <experience-timeline-item v-bind="item">
+    <template #content="{ item }">
+      <ExperienceTimelineItem v-bind="item">
         <div v-md="item.description" />
-      </experience-timeline-item>
+      </ExperienceTimelineItem>
     </template>
   </Timeline>
 </template>
--- app/utils/style.ts
+++ app/utils/style.ts
@@ -1,26 +1,27 @@
-export const variantsClasses = new Map()
-variantsClasses.set('primary', 'bg-green-600 hover:bg-green-700 text-slate-50')
+export const variantsClasses = new Map();
+variantsClasses.set("primary", "bg-green-600 hover:bg-green-700 text-slate-50");
 
-export const technologiesColorsClasses = new Map()
-technologiesColorsClasses.set('HTML', 'bg-orange-500')
-technologiesColorsClasses.set('CSS', 'bg-blue-500')
-technologiesColorsClasses.set('JavaScript', 'bg-yellow-500')
-technologiesColorsClasses.set('Vue', 'bg-green-600')
-technologiesColorsClasses.set('TailwindCSS', 'bg-cyan-500')
-technologiesColorsClasses.set('Laravel', 'bg-red-500')
-technologiesColorsClasses.set('PHP', 'bg-purple-400')
-technologiesColorsClasses.set('MySQL', 'bg-blue-600')
-technologiesColorsClasses.set('MongoDB', 'bg-green-500')
-technologiesColorsClasses.set('AWS', 'bg-orange-400')
-technologiesColorsClasses.set('Bootstrap', 'bg-purple-500')
-technologiesColorsClasses.set('CSS', 'bg-sky-500')
-technologiesColorsClasses.set('SCSS', 'bg-rose-600')
-technologiesColorsClasses.set('Node.js', 'bg-lime-600')
-technologiesColorsClasses.set('Webpack', 'bg-cyan-500')
-technologiesColorsClasses.set('Vite.js', 'bg-indigo-300')
-technologiesColorsClasses.set('Typescript', 'bg-blue-400')
-technologiesColorsClasses.set('Docker', 'bg-cyan-600')
-technologiesColorsClasses.set('Jest', 'bg-red-800')
-technologiesColorsClasses.set('Cypress', 'bg-slate-700')
-technologiesColorsClasses.set('C', 'bg-slate-400')
-technologiesColorsClasses.set('Java', 'bg-orange-700')
+export const technologiesColorsClasses = new Map();
+technologiesColorsClasses.set("HTML", "bg-orange-500");
+technologiesColorsClasses.set("CSS", "bg-blue-500");
+technologiesColorsClasses.set("JavaScript", "bg-yellow-500");
+technologiesColorsClasses.set("TypeScript", "bg-blue-700");
+technologiesColorsClasses.set("Vue", "bg-green-600");
+technologiesColorsClasses.set("TailwindCSS", "bg-cyan-500");
+technologiesColorsClasses.set("Laravel", "bg-red-500");
+technologiesColorsClasses.set("PHP", "bg-purple-400");
+technologiesColorsClasses.set("MySQL", "bg-blue-600");
+technologiesColorsClasses.set("MongoDB", "bg-green-500");
+technologiesColorsClasses.set("AWS", "bg-orange-400");
+technologiesColorsClasses.set("Bootstrap", "bg-purple-500");
+technologiesColorsClasses.set("CSS", "bg-sky-500");
+technologiesColorsClasses.set("SCSS", "bg-rose-600");
+technologiesColorsClasses.set("Node.js", "bg-lime-600");
+technologiesColorsClasses.set("Webpack", "bg-cyan-500");
+technologiesColorsClasses.set("Vite.js", "bg-indigo-300");
+technologiesColorsClasses.set("Typescript", "bg-blue-400");
+technologiesColorsClasses.set("Docker", "bg-cyan-600");
+technologiesColorsClasses.set("Jest", "bg-red-800");
+technologiesColorsClasses.set("Cypress", "bg-slate-700");
+technologiesColorsClasses.set("C", "bg-slate-400");
+technologiesColorsClasses.set("Java", "bg-orange-700");
