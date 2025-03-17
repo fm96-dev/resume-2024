4bae3b7a8b61928909c6570d0c313931ec5e495b
Federico Mameli <federico_mameli@yahoo.com>
2025-03-17 13:18:49 +0000

    chore: update tailwind.config.js for improved content paths and formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    '**/components/*.{vue,js,ts,jsx,tsx}',
-    '**/utils/*.{vue,js,ts,jsx,tsx}',
-    './node_modules/primevue/**/*.{vue,js,ts,jsx,tsx}'
+    "./app/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/primevue/**/*.{vue,js,ts,jsx,tsx}",
   ],
 
   theme: {
     container: {
-      padding: '0 1rem',
-      center: true
+      padding: "0 1rem",
+      center: true,
     },
-    extend: {}
+    extend: {},
   },
 
-  plugins: []
-}
+  plugins: [],
+};
